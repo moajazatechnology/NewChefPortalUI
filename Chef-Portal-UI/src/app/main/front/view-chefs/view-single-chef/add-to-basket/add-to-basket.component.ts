d634b04c6c6b5f6eed96a31b70633bd8a95cd6d6
muktasalunke-hash <69347246+muktasalunke-hash@users.noreply.github.com>
2021-09-03 12:45:54 +0530

    basket changes

--- Chef-Portal-UI/src/app/main/front/view-chefs/view-single-chef/add-to-basket/add-to-basket.component.ts
+++ Chef-Portal-UI/src/app/main/front/view-chefs/view-single-chef/add-to-basket/add-to-basket.component.ts
@@ -99,16 +99,16 @@ export class AddToBasketComponent implements OnInit {
         this.productTotalPrice = this.productTotalPrice + varient.options[idx].price;
       }else {
 
-        let limit = 0;
-        varient.options.forEach((element) => {
-          if(element.default){
-            limit = limit+1;
-          }
-        });
+        // let limit = 0;
+        // varient.options.forEach((element) => {
+        //   if(element.default){
+        //     limit = limit+1;
+        //   }
+        // });
 
-        console.log(limit);
+        // console.log(limit);
 
-        if(varient.max_selection >= limit) {
+        if(varient.max_selection > varient?.checkedOptions?.length) {
           this.productTotalPrice = this.productTotalPrice + varient.options[idx].price;
         }else {
           varient.options.forEach((element,index) => {
@@ -116,6 +116,17 @@ export class AddToBasketComponent implements OnInit {
               if(element.default){
                 element.default = false;
                 this.productTotalPrice = this.productTotalPrice - varient.options[idx].price;
+
+                this.checkedProductVariantOptions.forEach((variant,indx) => {
+                  variant?.checkedOptions.forEach((element,index) => {
+                    
+                    if(!element.default) {
+
+                      this.productTotalPrice = this.productTotalPrice - varient.options[idx].price;
+                      this.checkedProductVariantOptions[indx]['checkedOptions'].splice(index,1);
+                    }
+                  });
+                });
             
               }else {
 
@@ -136,29 +147,29 @@ export class AddToBasketComponent implements OnInit {
           varient.options[idx].default = true;
           this.productTotalPrice = this.product.price + varient.options[idx].price;
         }
-      }
-      let duplicateVariant = this.checkedProductVariantOptions.find(v => v.id === varient.id);
+        
+        let duplicateVariant = this.checkedProductVariantOptions.find(v => v.id === varient.id);
 
-      if(duplicateVariant) {
-        let index = 0;
-        this.checkedProductVariantOptions.forEach((element,indx) => {
-          if(element.id=== varient.id){
-            index = indx;
-          }
-        });
-        this.checkedProductVariantOptions[index]['checkedOptions'].push(varient.options[idx]);
-      }else{
-        this.checkedProductVariantOptions.push(varient);
-        let index = 0;
-        this.checkedProductVariantOptions.forEach((element,indx) => {
-          if(element.id=== varient.id){
-            index = indx;
-          }
-        });
-        console.log(this.checkedProductVariantOptions);
-        this.checkedProductVariantOptions[index]['checkedOptions'] = [(varient.options[idx])];
+        if(duplicateVariant) {
+          let index = 0;
+          this.checkedProductVariantOptions.forEach((element,indx) => {
+            if(element.id=== varient.id){
+              index = indx;
+            }
+          });
+          this.checkedProductVariantOptions[index]['checkedOptions'].push(varient.options[idx]);
+        }else{
+          this.checkedProductVariantOptions.push(varient);
+          let index = 0;
+          this.checkedProductVariantOptions.forEach((element,indx) => {
+            if(element.id=== varient.id){
+              index = indx;
+            }
+          });
+          console.log(this.checkedProductVariantOptions);
+          this.checkedProductVariantOptions[index]['checkedOptions'] = [(varient.options[idx])];
+        }
       }
-
     }
 
   }
